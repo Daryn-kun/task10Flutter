5a59469e26e6697528b8d4b315e56061592c9131
Daryn-kun <daryn.123434@gmail.com>
2021-04-24 18:21:36 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:form_app/pages/register_form_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Register Form Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RegisterFormPage(),
+    );
+  }
+}
+
